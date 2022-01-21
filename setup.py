f6f124369ab5b1779b31ec37debe8264f96f1d08
Azzeddine TIBA <azzeddine.tiba@gmail.com>
2022-01-22 00:55:59 +0100

    Add setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="rom_am",
-    packages=find_packages(),
+   name='ROM_AM',
+   version='0.1',
+   description='Non-intrusive Reduced Order Modeling packages',
+   author='TIBA Azzeddine',
+   author_email='azzeddine.tiba@lecnam.net',
+   packages=['rom_am'], 
+   install_requires=['numpy', 'scipy',], #external packages as dependencies
 )
