9a1608799525e53010c34080dbe14967f9edb503
Azzeddine TIBA <azzeddine.tiba@gmail.com>
2022-01-22 01:00:41 +0100

    Add jax in setup py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import find_packages, setup
 
 
 setup(
-   name='ROM_AM',
-   version='0.1',
-   description='Non-intrusive Reduced Order Modeling packages',
-   author='TIBA Azzeddine',
-   author_email='azzeddine.tiba@lecnam.net',
-   packages=['rom_am'], 
-   install_requires=['numpy', 'scipy',], #external packages as dependencies
+    name='ROM_AM',
+    version='0.1',
+    description='Non-intrusive Reduced Order Modeling packages',
+    author='TIBA Azzeddine',
+    author_email='azzeddine.tiba@lecnam.net',
+    packages=['rom_am'],
+    # external packages as dependencies
+    install_requires=['numpy >= 1.20.3', 'scipy >= 1.7.1',
+                      'jax >= 0.2.26 ; platform_system=="linux"'],
 )
