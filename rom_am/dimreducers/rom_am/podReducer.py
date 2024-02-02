Fix annoying normalization error

rom_am/dimreducers/rom_am/podReducer.py
@@ -12,7 +12,7 @@ class PodReducer(RomDimensionalityReducer):
     def __init__(self, latent_dim, ) -> None:
         super().__init__(latent_dim)
 
-    def train(self, data, map_used=None, normalize=True, center=True, alg="svd", to_copy=True):
+    def train(self, data, map_used=None, normalize=True, center=True, alg="svd", to_copy=True, to_copy_order='F'):
 
         super().train(data, map_used)
 
@@ -20,7 +20,7 @@ class PodReducer(RomDimensionalityReducer):
         rom = ROM(pod)
 
         rom.decompose(X=data, normalize=normalize, center=center,
-                      rank=self.latent_dim, alg=alg, to_copy=to_copy)
+                      rank=self.latent_dim, alg=alg, to_copy=to_copy, to_copy_order=to_copy_order)
 
         self.latent_dim = pod.kept_rank
         self.normalize = normalize
@@ -29,7 +29,10 @@ class PodReducer(RomDimensionalityReducer):
         self.pod = pod
 
         if map_used is not None:
-            self.interface_dim = map_used.shape[0]
+            if map_used.dtype == int:
+                self.interface_dim = map_used.shape[0]
+            else:
+                self.interface_dim = len(np.argwhere(map_used))
             self.map_mat = map_used
 
         """
@@ -60,14 +63,8 @@ class PodReducer(RomDimensionalityReducer):
 
         if self.map_mat is not None and not high_dim:
             interm = self._mapped_decode(new_data)
-            if self.center:
-                interm = (
-                    interm + self.rom.mean_flow[self.map_mat].reshape(
-                        (-1, 1)))
-            return interm
-
+            return self.rom.decenter(self.rom.denormalize(interm, self.map_mat), self.map_mat)
         else:
-
             interm = self.pod.inverse_project(new_data)
             return self.rom.decenter(self.rom.denormalize(interm))
 
@@ -75,8 +72,7 @@ class PodReducer(RomDimensionalityReducer):
         return POD()
 
     def _mapped_decode(self, new_data):
-        return self.rom.denormalize(
-            self.pod.modes)[self.map_mat, :] @ new_data
+        return self.pod.modes[self.map_mat, :] @ new_data
 
     @property
     def reduced_data(self):

rom_am/rom.py
@@ -59,6 +59,7 @@ class ROM:
             normalization="norm",
             norm_info=None,
             to_copy=True,
+            to_copy_order='F',
             *args,
             **kwargs,):
         """Computes the data decomposition, training the model on the input data X.
@@ -121,15 +122,15 @@ class ROM:
 
         if to_copy:
             self.to_copy=True
-            self.snapshots=X.copy()
+            self.snapshots=X.copy(order=to_copy_order)
             snapshots=self.snapshots
         else:
             snapshots=X
         self.nx = X.shape[0]
         if "Y" in kwargs.keys():
-            self.Y = kwargs["Y"].copy()
+            self.Y = kwargs["Y"].copy(order=to_copy_order)
             if "Y_input" in kwargs.keys():
-                self.Y_input = kwargs["Y_input"].copy()
+                self.Y_input = kwargs["Y_input"].copy(order=to_copy_order)
         if center:
             self.center_ = center
             self._center(snapshots)
@@ -140,7 +141,7 @@ class ROM:
             self.normalization = normalization
             if self.norm_info is not None:
                 self.normalization = "spec"
-            self._normalize(snapshots)
+            self._normalize(self.center(snapshots))
         if "Y" in kwargs.keys():
             kwargs["Y"] = self.Y
             if "Y_input" in kwargs.keys():
@@ -303,7 +304,7 @@ class ROM:
                     np.repeat(self.norm_info[:, 0], self.norm_info[:, 1].astype(int))[
                         :, np.newaxis]
 
-    def denormalize(self, res):
+    def denormalize(self, res, ids=None):
         """denormalization of the input array
 
         Parameters
@@ -321,13 +322,16 @@ class ROM:
                 return res * self.max_min + self.snap_min[:, np.newaxis]
             elif self.normalization == "norm":
                 if self.Y_input is not None:
-                    dirichletNorms = self.snap_norms[:self.nx, np.newaxis].copy()
-                    dirichletNorms[self.zeroIds] = 0.
-                    return res * dirichletNorms
+                    res_ = res * self.snap_norms[:self.nx, np.newaxis]
+                    res_[self.zeroIds] = 0.
+                    return res_
                 else:
-                    dirichletNorms = self.snap_norms[:, np.newaxis].copy()
-                    dirichletNorms[self.zeroIds] = 0.
-                    return res * dirichletNorms
+                    if ids is None:
+                        res_ = res * self.snap_norms[:, np.newaxis]
+                        res_[self.zeroIds] = 0.
+                    else:
+                        res_ = res * self.snap_norms[ids, np.newaxis]
+                    return res_
             elif self.normalization == "spec":
                 return res * np.repeat(self.norm_info[:, 0], self.norm_info[:, 1].astype(int))[
                     :, np.newaxis]
@@ -357,7 +361,7 @@ class ROM:
         if self.to_copy:
             self.snapshots = self.center(snaps)
 
-    def decenter(self, res):
+    def decenter(self, res, ids=None):
         """Decenter the data on the input array
 
         Parameters
@@ -371,7 +375,10 @@ class ROM:
             the decentered data based on the mean of the input snapshots
         """
         try:
-            return res + self.mean_flow.reshape((-1, 1))
+            if ids is None:
+                return res + self.mean_flow.reshape((-1, 1))
+            else:
+                return res + self.mean_flow[ids].reshape((-1, 1))
         except AttributeError:
             return res
     def reconstruct(self, rank=None):

rom_am/solid_rom.py
@@ -38,9 +38,12 @@ class solid_ROM:
               forcesReduc_model=None,
               dispReduc_model=None,
               norm_regr=[False, False],
+              norm_dimRed=[True, True],
+              center_dimRed=[True, True],
               norm=["l2", "l2"],
               algs=["svd", "svd"],
               to_copy=[True, True],
+              to_copy_order=['F', 'F'],
               remove_outliers=False,
               previous_disp_data=None):
         """Training the solid ROM model
@@ -94,16 +97,24 @@ class solid_ROM:
             Whether to normalize the inputs and outputs of the regression
             model. The normalization used is chosen by the `norm`argument
             Default : [True, True]
+        norm_dimRed  : list of 2 booleans, optional
+            Default : [True, True]
+        center_dimRed  : list of 2 booleans, optional
+            Default : [True, True]
         norm       : list of 2 strs, optional
             Type of normalization used ([inputs, outputs]).
             "minmax" for min-max normalization. "l2" for L2
             normalization. "std" for standardization.
             Default : ["minmax", "minmax"]
-        norm       : list of 2 strs, optional
+        algs       : list of 2 strs, optional
             Type of decomposition used ([inputs, outputs]).
             Whether to use the SVD on decomposition ("svd") or
             the eigenvalue problem on snaphot matrices ("snap")
             Default : ["svd", "svd"]
+        to_copy  : list of 2 booleans, optional
+            Default : [True, True]
+        to_copy_order  : list of 2 booleans, optional
+            Default : [True, True]
 
         Returns
         ------
@@ -136,11 +147,13 @@ class solid_ROM:
         else:
             self.dispReduc_model = dispReduc_model
 
-        self.dispReduc_model.train(used_disp_data, map_used=map_used, alg = algs[1], to_copy=to_copy[1])
+        self.dispReduc_model.train(used_disp_data, map_used=map_used,
+                                   alg=algs[1], to_copy=to_copy[1], center=center_dimRed[1], normalize=norm_dimRed[1], to_copy_order=to_copy_order[1])
         disp_coeff = self.dispReduc_model.reduced_data
 
         if self.is_dynamical:
-            previous_disp_coeff = self.dispReduc_model.encode(previous_disp_data)
+            previous_disp_coeff = self.dispReduc_model.encode(
+                previous_disp_data)
         else:
             previous_disp_coeff = None
 
@@ -152,7 +165,8 @@ class solid_ROM:
         else:
             self.forcesReduc = forcesReduc_model
 
-        self.forcesReduc.train(used_pres_data, alg = algs[0], to_copy=to_copy[0])
+        self.forcesReduc.train(used_pres_data, alg=algs[0], to_copy=to_copy[0],
+                               normalize=norm_dimRed[0], center=center_dimRed[0], to_copy_order=to_copy_order[0])
         pres_coeff = self.forcesReduc.reduced_data
 
         if ids is not None:
@@ -251,9 +265,6 @@ class solid_ROM:
                 unus_pres_coeff = (unus_pres_coeff -
                                    pres_coeff_min) / (pres_coeff_max_min)
 
-        # self.saved_pres_coeff = pres_coeff.copy()
-        # self.saved_disp_coeff = diameter_coeff.copy()
-
         # ========= Regression =========
         if ids is None:
             pres_coeff_tr = pres_coeff
@@ -292,12 +303,12 @@ class solid_ROM:
         if not self.is_dynamical:
             self.regressor.train(pres_coeff_tr, disp_coeff_tr)
         else:
-            self.regressor.train(pres_coeff_tr, disp_coeff_tr, previous_disp_coeff)
-        # self.saved_prs_cf_tr = pres_coeff_tr.copy()
-        # self.saved_disp_cf_tr = disp_coeff_tr.copy()
+            self.regressor.train(
+                pres_coeff_tr, disp_coeff_tr, previous_disp_coeff)
 
     def reject_outliers(self, data, m=8):
-        ids_ = np.max((np.abs(data - np.mean(data, axis=1).reshape((-1, 1)))), axis = 0) < m*np.std(data, axis = 0)
+        ids_ = np.max((np.abs(data - np.mean(data, axis=1).reshape((-1, 1)))),
+                      axis=0) < m*np.std(data, axis=0)
         return data[:, ids_], ids_
 
     def pred(self, new_pres, previous_disp=None, alpha=None):
@@ -341,24 +352,28 @@ class solid_ROM:
             if self.norm_regr[1]:
                 if self.norms[0] == "minmax":
                     previous_disp_coeff = (previous_disp_coeff -
-                                    self.disp_coeff_min) / self.disp_coeff_max_min
+                                           self.disp_coeff_min) / self.disp_coeff_max_min
                 elif self.norms[0] == "l2":
                     previous_disp_coeff = (previous_disp_coeff -
-                                    self.disp_coeff_mean) / self.disp_coeff_nrm
+                                           self.disp_coeff_mean) / self.disp_coeff_nrm
                 elif self.norms[0] == "std":
                     previous_disp_coeff = (previous_disp_coeff -
-                                    self.disp_coeff_mean) / self.disp_coeff_std
+                                           self.disp_coeff_mean) / self.disp_coeff_std
 
-        # self.saved_pres_pred_coeff = pred_pres_coeff.copy()
         # ============== Regression predicts =====================
         t2 = time.time()
         self.regressor.check_predict_in(pred_pres_coeff)
         if not self.is_dynamical:
             res1 = self.regressor.predict(pred_pres_coeff)
         else:
-            res1 = self.regressor.predict(pred_pres_coeff, previous_disp_coeff, alpha=alpha)
+            res1 = self.regressor.predict(
+                pred_pres_coeff, previous_disp_coeff, alpha=alpha)
         self.regressor.check_predict_out(res1)
         t3 = time.time()
+
+        # ============== Check if new point is close to seen data =====================
+        # self.dispReduc_model._check_encode_nearness(res1)
+        # res1 = self.dispReduc_model.correct_far_point(res1)
 
         # ============== Denormalize Displ. coefficients =====================
         if self.norm_regr[1]: